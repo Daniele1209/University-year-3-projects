cf1659ba578600cc4f48ff9059f3e443d4416ae8
Mos Daniele <mdie2771@scs.ubbcluj.ro>
2021-10-19 10:55:28 +0300

    PDP hw1 -> done

--- Parallel and distributed programming/Lab1_multi-threading/Bank_accounts.py
+++ Parallel and distributed programming/Lab1_multi-threading/Bank_accounts.py
@@ -1,5 +1,6 @@
 import logging 
 import threading 
+from threading import Lock
 import time
 from Account import Account
 from Operation import Operation
@@ -19,63 +20,101 @@ From time to time, as well as at the end of the program, a consistency check sha
   on each account appear also in the logs of the source or destination of the transfer.
 """
 
+repository = Repository()
+TRANSACTION_NUMBER = 10
+THREAD_NUMBER = 100
+ACCOUNT_NUMBER = 40
+CONSISTENCY_CHECK = 10
+consistency_mutex = Lock()
+failed_passes = 0
+
 def executeConsistencyCheck():
-    
+    consistency_mutex.acquire()
+    print('Started consistency check !')
+    # Make a list of booleans to keep count of the consistency
+    consistencyList = {}
+    accountsList = repository.getAccounts()
+    # Iterate through all the accouns
+    for account in accountsList:
+        initialAccountBalance = repository.getInitialBalance(account)
+        currentAccountBalance = account.getBalance()
+        accountLogs = account.getLogs()
+        # Go through all of the account logs and process the operations to see if we end up with the current balance
+        for log in accountLogs.values():
+            log_split = log.split(',')
+            if log_split == 'send':
+                initialAccountBalance -= int(log_split[1].split(':')[1])
+            else:
+                initialAccountBalance += int(log_split[1].split(':')[1])
+            if initialAccountBalance == currentAccountBalance:
+                consistencyList[account.getName()] = True
+    if False not in consistencyList.values():
+        print('Consistency check passed !')
+    else:
+        print('Consistency check failed !')
+        failed_passes += 1
+    consistency_mutex.release()
+
 
 def executeAddOperation(operation, accountX, accountY):
     amount = operation.getAmount()
-    accountX.subtractBalance(amount)
-    transactionId, transactionLog = repository.appendLog(operation, amount, accountX, accountY)
-    accountX.appendLog(transactionId, transactionLog)
+    generatedId = repository.generateId()
+    accountY.addBalance(amount, accountX, generatedId)
+    transactionLog = repository.appendLog(accountX.getLog(generatedId), generatedId)
     print(transactionLog)
 
 def executeSubtractionOperation(operation, accountX, accountY):
     amount = operation.getAmount()
-    accountX.addBalance(amount)
-    transactionId, transactionLog = repository.appendLog(operation, amount, accountX, accountY)
-    accountX.appendLog(transactionId, transactionLog)
+    generatedId = repository.generateId()
+    accountX.subtractBalance(amount, accountY, generatedId)
+    transactionLog = repository.appendLog(accountX.getLog(generatedId), generatedId)
     print(transactionLog)
 
+def threadTransaction(operation, accountX, accountY):
+    try:
+        amount = operation.getAmount()
+        generatedId = repository.generateId()
+        accountX.subtractBalance(amount, accountY, generatedId)
+        transactionLog = repository.appendLog(accountX.getLog(generatedId), generatedId)
+        print(transactionLog)
+        accountY.addBalance(amount, accountX, generatedId)
+        transactionLog = repository.appendLog(accountY.getLog(generatedId), generatedId)
+        print(transactionLog)
+    except Exception:
+        print('Balance not sufficient !')
+
+def threadFunctionLoop(nb_of_loops):
+    for idx in range(nb_of_loops):
+        transferOperation = Operation('add', random.randint(1, 300))
+        accountX, accountY = random.sample(set(repository.getAccounts()), 2)
+        threadTransaction(transferOperation, accountX, accountY)
+
 if __name__ == '__main__':
-    repository = Repository()
-    accountList = []
 
     # Declare the user accounts
-    account1 = Account('Name 1')
-    account2 = Account('Name 2')
-    account3 = Account('Name 3')
-
     # Append accounts to list in order to make transactions between them at random
-    accountList.append(account1)
-    accountList.append(account2)
-    accountList.append(account3)
-
     # Adding initial balance to repo in order to keep track of initial balance
-    repository.appendInitialBalance(account1)
-    repository.appendInitialBalance(account2)
-    repository.appendInitialBalance(account3)
+    for account_idx in range(1, ACCOUNT_NUMBER):
+        new_account = Account('Name ' + str(account_idx))
+        repository.appendAccount(new_account)
+        repository.appendInitialBalance(new_account)
+    
+    print('Accounts generated successfully!')
 
-    # Declare the number of total transactions and the step at which the consistency check will take place
-    numberOfOperations = 10
-    consistencyCheck = 3
+    start = time.time()
 
-    for operationIndex in range(numberOfOperations):
+    for threadIndex in range(THREAD_NUMBER):
 
-        if operationIndex % consistencyCheck == 0:
+        if threadIndex % CONSISTENCY_CHECK == 0:
             executeConsistencyCheck()
         else:
-            transferOperation = Operation('add', random.randint(1, 300))
-            subtractOpreation = Operation('subtract', transferOperation.getAmount())
-            accountX, accountY = random.sample(set(accountList), 2)
-            # Start a thread for each of the 2 transactions
-            if transferOperation.getOperationType() == 'add':
-                t1 = threading.Thread(target = executeAddOperation(transferOperation, accountX, accountY), args=(1, ))
-                t2 = threading.Thread(target = executeSubtractionOperation(subtractOpreation, accountY, accountX), args=(1, ))
-                t1.start()
-                t2.start()
-                t1.join()
-                t2.join()
-            
-    print('Account 1 final balance: ' + str(account1.getBalance()))
-    print('Account 2 final balance: ' + str(account2.getBalance()))
-    print('Account 3 final balance: ' + str(account3.getBalance()))
+            #transferOperation = Operation('add', random.randint(1, 300))
+            #accountX, accountY = random.sample(set(repository.getAccounts()), 2)
+            # Start a thread for a transaction between the 2 random selected accounts
+            #t = threading.Thread(target = threadTransaction(transferOperation, accountX, accountY), args=(threadIndex, ))
+            t = threading.Thread(target = threadFunctionLoop(TRANSACTION_NUMBER), args=(threadIndex, ))
+            t.start()
+    t.join()
+    elapsed_time = time.time() - start
+    print('Elapsed time: ' + str(elapsed_time))
+    print('Failed consistency checks: ' + str(failed_passes))
--- Parallel and distributed programming/Lab1_multi-threading/Repository.py
+++ Parallel and distributed programming/Lab1_multi-threading/Repository.py
@@ -5,17 +5,36 @@ class Repository:
     def __init__(self):
         self.logHistory = {}
         self.initialBalances = {}
-        self.lastId = 0
+        self.log_filename = 'log_file.txt'
+        self.accounts = []
+        self.lastId = -1
+        self.cleanLog()
 
-    def appendLog(self, operation, amount, accountX, accountY):
-        idGeneration = random.randint(1, 1000)
-        while idGeneration in self.logHistory.keys():
-            idGeneration = random.randint(1, 1000)
-        self.lastId = idGeneration
-        logString = str(operation.getOperationType()) + ' Account 1: ' + str(accountX.getName()) + ' Account 2: ' + str(accountY.getName()) + ' Amount: ' + str(amount)
-        self.logHistory[idGeneration] = logString
-        
-        return self.lastId, logString
+    def appendLog(self, logString, id):
+        self.logHistory[id] = logString
+        with open(self.log_filename, 'a') as file:
+            file.write(logString)
+            file.write('\n')
+
+        return logString
+
+    def cleanLog(self):
+        open(self.log_filename, 'w').close()
+    
+    def appendAccount(self, account):
+        self.accounts.append(account)
+    
+    def getAccounts(self):
+        return self.accounts
+
+    def getInitialBalance(self, account):
+        return self.initialBalances[str(account.getName())]
 
     def appendInitialBalance(self, account):
         self.initialBalances[str(account.getName())] = account.getBalance()
+
+    def generateId(self):
+        idGeneration = self.lastId + 1
+        self.lastId += 1
+
+        return idGeneration

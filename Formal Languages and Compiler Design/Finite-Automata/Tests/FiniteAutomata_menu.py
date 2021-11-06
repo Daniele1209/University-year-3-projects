c2299b31f95866f1db4a2274ac31982af23aac3e
Mos Daniele <mdie2771@scs.ubbcluj.ro>
2021-11-06 21:14:14 +0200

    FDLC finite automata -> update

--- Formal Languages and Compiler Design/Finite-Automata/Tests/FiniteAutomata_menu.py
+++ Formal Languages and Compiler Design/Finite-Automata/Tests/FiniteAutomata_menu.py
@@ -4,19 +4,41 @@ def printMenu(commands):
     print('== Commands ==')
     for comm in commands.keys():
         print('>> ' + comm)
+    print('>> check dfa')
+    print('>> accepted')
     print('>> exit')
 
-if __name__ == '__main__':
-    FA = FiniteAutomata()
-    FA.read('../Programs/FA.in')
-    items = FA.getData()
 
-    commands = {'states': items['states'], 'alphabet': items['alphabet'], 'transitions': items['transitions'], 'final states': items['final states']}
+def checkDfa():
+    if FA.checkDFA():
+        print('FA is DFA')
+    else:
+        print('FA is not DFA')
+
+
+def isAccepted():
+    input_seq = input('enter sequence: ')
+    if FA.acceptedSequence(input_seq):
+        print('sequence accepted')
+    else:
+        print('sequence not accepted')
+
+FA = FiniteAutomata()
+FA.read('../Programs/FA.in')
+items = FA.getData()
+commands = {'states': items['states'], 'alphabet': items['alphabet'],
+                'transitions': items['transitions'], 'final states': items['final states']}
+
+if __name__ == '__main__':
     printMenu(commands)
 
     command = input('>> ')
     while command != 'exit':
-        if command in commands.keys():
+        if command == 'check dfa':
+            checkDfa()
+        elif command == 'accepted':
+            isAccepted()
+        elif command in commands.keys():
             print(commands[command])
         else:
             print('Command not valid !')
